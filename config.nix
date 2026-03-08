cf6fe5cb950d9c3d50f632dd587fb072fedab368
Enzo Pacheco <55354168+Packss@users.noreply.github.com>
2026-03-08 18:44:19 -0300

    Update keymaps and shields in config.nix

--- config.nix
+++ config.nix
@@ -4,18 +4,13 @@ with pkgs.lib;
 
 {
 	keymaps = {
-		# "shields/corne" = pkgs.fetchgit {
-		# 	url = "https://github.com/diamondburned/zmk-config-corne";
-		# 	rev = "c22619ab76f26a3bf9b0fa14955e10db3c5ac62c";
-		# 	sha256 = "014lb6bv40d627xh9j58vi0516wvwwfqd1mn148pz17jml8kq2mc";
-		# } + "/config/corne.keymap";
-		"shields/corne" = "${../zmk-config-corne/config}/corne.keymap";
+		"shields/sofle" = "${../zmk-config-sofle/config}/sofle.keymap";
 	};
 	build = {
 		board = "nice_nano";
 		shields = [
-			"corne_left"
-			"corne_right"
+			"sofle_left"
+			"sofle_right"
 		];
 	};
 }
